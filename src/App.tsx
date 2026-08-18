9bd3bae759f6055bf749e5dc3e520e26b495a739
OlyaJ <schastnayaolga92@gmail.com>
2026-08-18 22:14:00 +0300

    ci: added frontend checks workflow

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Button } from './shared/ui/button';
 function App() {
   return (
     <main className="p-10">
-      <h1 className="mb-3 text-3xl font-bold">FlowBoard</h1>
-      <Button>Create a new app</Button>
+      <h1 className="mb-3 text-3xl font-bold">Vocab Gym</h1>
+      <Button>Log in</Button>
     </main>
   );
 }
